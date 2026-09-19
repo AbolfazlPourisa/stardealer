348de9046d7b9b1ebe46d174f67cbc51e89f70cc
AbolfazlPourisa <AbolfazlPourisa@gmail.com>
2026-09-19 18:19:07 +0330

    feat(bot): Config database and migraions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-function main() {
+import { Database } from "./database/connection.js"; 
+import { Migrations } from "./database/migrations.js";
+import Config from "./config/config.js";
+
+async function main() {
+    let migrations = new Migrations("./src/migrations");
+
+    let database = new Database(
+        Config.dbHost,
+        Config.dbPort,
+        Config.dbUser,
+        Config.dbPassword,
+        Config.dbName
+    );
+
+    await database.connect();
+    await database.test();
+    await database.createTables(migrations.config);
 }
 
 main();
